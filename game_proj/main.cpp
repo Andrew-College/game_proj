c2e13e3d38b651051f46b30a1b94882e92641f55
Andrew <d00133579@student.dkit.ie>
2012-09-30 21:00:29 +0100

    Fixed welcome colours, fixed for loop spam, working highscore tab, added more fun

--- game_proj/main.cpp
+++ game_proj/main.cpp
@@ -10,7 +10,8 @@ using namespace std;
 int main_menu();
 int welcome();
 int highScore();
-int highScore_score;
+int highScore_score();
+
 vector <int> score;
 
 ///////////////////////////////////////////
@@ -27,16 +28,18 @@ int main_menu(){
 	int choice = 0;
 	bool end = false;
 	welcome();
-	cout << "MINI GOLF MANIA" << endl << endl;
-	cout << "Main Menu" << endl << "1. How to play." << endl << "2. Play Now!" << endl <<"3. High Scores." << endl <<"4. Quit"<<endl<<endl;
-	cin >> choice;
+
 	do{
+		choice = 0;
+		cout << "MINI GOLF MANIA" << endl << endl;
+		cout << "Main Menu" << endl << "1. How to play." << endl << "2. Play Now!" << endl <<"3. High Scores." << endl <<"4. Quit"<<endl<<endl;
+		cin >> choice;
 		switch (choice){
 		case 1:
-			tutorial();
+			//tutorial();
 			break;
 		case 2:
-			game();
+			//game();
 			break;
 		case 3:
 			highScore();
@@ -48,11 +51,26 @@ int main_menu(){
 int welcome(/*Say hello to the nice people*/){
 	system("Color 0C");
 	cout << "WELCOME TO MINI GOLF MANIA"<<endl<<endl;
+	Sleep(200);
 	system("Color 0F");
+	Sleep(200);
+	system("Color 0C");
+	Sleep(200);
+	system("Color 0F");
+	Sleep(200);
 	Sleep(1000);
 	cout << "Play smart!"<< endl;
+	Sleep(200);
+	system("Color 0C");
+	Sleep(200);
+	system("Color 0F");
+	Sleep(200);
 	Sleep(1000);
 	cout <<"Win low!"<<endl;
+	Sleep(200);
+	system("Color 0C");
+	Sleep(200);
+	system("Color 0F");
 	Sleep(4000);
 	system("cls");
 	Sleep(500);
@@ -60,4 +78,17 @@ int welcome(/*Say hello to the nice people*/){
 }
 
 int highScore(){
+	if(score.size()!=0){
+		for(int i = 0; i < score.size(); i++){
+			cout << /*placeholder for names <<*/ score.at(i)<< flush <<endl;
+			Sleep(300);
+		}
+	}else{
+		cout << "no scores here yet!" << endl;
+	}
+	Sleep(4000);
+	cout << "returning to main menu"<< endl;
+	Sleep(2000);
+	system("cls");
+	return 0;
 }
