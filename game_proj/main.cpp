1c0e2a5a49b4c030f26c0f950e7620731af9c924
Andrew <d00133579@student.dkit.ie>
2012-09-30 14:45:01 +0100

    beginning to learn menus, etc.

--- game_proj/main.cpp
+++ game_proj/main.cpp
@@ -1,9 +1,30 @@
 #include <stdio.h>
-#include<conio.h>
-
-
+#include <conio.h>
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+int main_menu();
+int welcome();
 
 int main(){
-	
+	main_menu();
+	return 0;
+}
+int main_menu(){
+	int choice = 0;
+	welcome();
+	return 0;
+}
+int welcome(/*Say hello to the nice people*/){
+	system("Color 0C");
+	cout << "WELCOME TO MINI GOLF MANIA"<<endl<<endl;
+	system("Color 0F");
+	Sleep(1000);
+	cout << "Play smart!"<< endl;
+	Sleep(1000);
+	cout <<"Win low!"<<endl;
+	Sleep(4000);
+	system("cls");
+	Sleep(500);
 	return 0;
 }
