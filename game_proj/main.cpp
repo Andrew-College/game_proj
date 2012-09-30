e2da9fe49fa9acf8d79de4f16e131e3e15981720
Andrew <d00133579@student.dkit.ie>
2012-09-30 19:30:48 +0100

    Initial menus<untested>, discovered c++ version of ArrayList<Vector>

--- game_proj/main.cpp
+++ game_proj/main.cpp
@@ -1,27 +1,48 @@
+//pre-compile Includes
 #include <stdio.h>
 #include <conio.h>
 #include <iostream>
 #include <Windows.h>
+#include <vector>
+//namespace section
 using namespace std;
+//declaring variables
 int main_menu();
 int welcome();
+int highScore();
+int highScore_score;
+vector <int> score;
 
+///////////////////////////////////////////
+//All the method names are human-readable//
+///////////////////////////////////////////
+
+//The main part, the bit that calls everything
 int main(){
 	main_menu();
 	return 0;
 }
+
 int main_menu(){
 	int choice = 0;
+	bool end = false;
 	welcome();
 	cout << "MINI GOLF MANIA" << endl << endl;
-	cout << "Main Menu" << endl << "1. How to play." << endl << "2. Play Now!" << endl <<"3. High Scores." << endl <<"4. Quit";
+	cout << "Main Menu" << endl << "1. How to play." << endl << "2. Play Now!" << endl <<"3. High Scores." << endl <<"4. Quit"<<endl<<endl;
+	cin >> choice;
 	do{
-	switch (choice){
-	case 1:
-		
-	
-	}
-
+		switch (choice){
+		case 1:
+			tutorial();
+			break;
+		case 2:
+			game();
+			break;
+		case 3:
+			highScore();
+			break;
+		}
+	}while(choice != 4);
 	return 0;
 }
 int welcome(/*Say hello to the nice people*/){
@@ -36,4 +57,7 @@ int welcome(/*Say hello to the nice people*/){
 	system("cls");
 	Sleep(500);
 	return 0;
+}
+
+int highScore(){
 }
