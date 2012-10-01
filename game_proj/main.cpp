539e867902b37aaffe17bd0458f0921a9e9df8e8
Andrew <d00133579@student.dkit.ie>
2012-10-02 00:04:27 +0100

    Began map testing, need to sort out integers, make for loops more readable, setup legend for characters, commented out isdigit<try out durzey thing>

--- game_proj/main.cpp
+++ game_proj/main.cpp
@@ -15,8 +15,27 @@ int tutorial();
 int highScore();
 int highScore_score();
 int test;
-bool isInt();
-vector <int> score;
+int game();
+void drawMap();
+int i = 0;
+int j = 0;
+//The map elements
+char ball = '*';//Your golf ball
+char hole = '#';
+char O_O_B = 'H';//Out of bounds area (should surround area)
+char diagWallF = '/';
+char diagWallB = '\\';//The backslash is an escape character, so it needs to be escaped
+string vertWall = "||";//It is a double to distinguish it from the direction selector
+char horizWall = '_';
+//end of map elements
+//position of objects in map
+int xBall;
+int yBall;
+int xHole;
+int yHole;
+//end of position objects
+
+vector <int> score;//the ArrayList the scores will be stored in
 
 ///////////////////////////////////////////
 //All the method names are human-readable//
@@ -39,13 +58,13 @@ int main_menu(){
 		cout << "MINI GOLF MANIA" << endl << endl;
 		cout << "Main Menu" << endl << "1. How to play." << endl << "2. Play Now!" << endl <<"3. High Scores." << endl <<"4. Quit"<<endl<<endl;
 		cin >> choice;
-		if(isdigit(choice)){
+		if(/*isdigit(choice) thi is commented out because its causing the code to stop, even with ints*/ 1==1){
 			switch (choice){
 			case 1:
 				tutorial();
 				break;
 			case 2:
-				//game();
+				game();
 				break;
 			case 3:
 				highScore();
@@ -90,32 +109,43 @@ int welcome(/*Say hello to the nice people*/){
 //Where the menu switchs are sent
 int tutorial(){
 	/*Load tutorial map*/
+	system("cls");
 	string welcome = "Welcome to the";
 	for(int i = 0; i < welcome.size(); i++){
-		cout << welcome[i] << endl;
-		Sleep(100);
+		cout << welcome[i];
+		Sleep(150);
 	}
 	system("cls");
 
 	cout << "MINI GOLF MANIA";
-	system("color 04");
-	Sleep(50);
-	system("color 31");
-	Sleep(50);
-	system("color F0");
-	Sleep(50);
-	system("color 79");
-	Sleep(50);
-	system("color 04");
-	Sleep(50);
-	system("color 31");
-	Sleep(50);
-	system("color F0");
-	Sleep(50);
-	system("color 79");
-	Sleep(50);
+	for(int i = 0; i < 4; i++){
+		system("color 04");
+		Sleep(50);
+		system("color 31");
+		Sleep(50);
+		system("color F0");
+		Sleep(50);
+		system("color 79");
+		Sleep(50);
+		system("color 04");
+		Sleep(50);
+		system("color 31");
+		Sleep(50);
+		system("color F0");
+		Sleep(50);
+		system("color 79");
+		Sleep(50);
+	}
 	system("cls");
 	system("Color 0F");
+	cout << "tutorial";
+	Sleep(800);
+	return 0;
+}
+int game(){
+	for(int i = 0; i <= 13; i++){
+		drawMap();
+	}
 	return 0;
 }
 int highScore(){
@@ -133,4 +163,27 @@ int highScore(){
 	system("cls");
 
 	return 0;
+}
+void drawMap(/*int xBall, int yBall, map <will be used, when I learn to read .txts>, int xHole, int yHole*/){
+
+	/*for(i = 0; i < 13; i++){
+		//not using the x,y coords here, this is just testing
+		cout << std::string(20,horizWall) << endl;
+		cout << vertWall << std::string(18," ") << vertWall <<endl;
+		cout<< vertWall << std::string(18," ") << vertWall <<endl;
+		cout<< vertWall << std::string(18," ") << vertWall <<endl;
+		cout<< vertWall << std::string(18," ") << vertWall <<endl;
+		cout<< vertWall << std::string(18," ") << vertWall <<endl;
+		cout<< vertWall << std::string(18," ") << vertWall <<endl;
+		cout<< vertWall << std::string(18," ") << vertWall <<endl;
+		cout<< vertWall << std::string(18," ") << vertWall <<endl;
+		cout<< std::string(20,horizWall) << endl;
+	} doesnt't work, Overload error and have to set i in the for loop*/
+	//this is just to test the direction selector
+	/*for(i = 0; i < map height; i++){
+		for(j = 0; j < map length; i++){
+			//in here there needs to be the accross stuff some of the walls, ball and hole, dont worry future me, them rats are churning
+			run through the map, line by line and print whatever the cursor is currently over, skipping if a particular thing <the ball> is in that x,y coord
+		}
+	}*/
 }
